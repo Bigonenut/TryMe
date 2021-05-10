8231c0a9771b2a7453229ca81273a287e88efd16
bigonenut <1729473054@qq.com>
2021-05-10 22:43:40 +0800

    1

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 
+ * @package Tryme
+ * @author bignut
+ * @version 1.0
+ * @link http://memax.top
+ */
+
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+ $this->need('header.php');
+ ?>
